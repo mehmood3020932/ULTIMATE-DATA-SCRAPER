b75216cbc4ba46f26a01016e6a22c163b88a53ef
mehmood khan <mehmood3020932@gmail.com>
2026-03-31 03:52:09 +0500

    Initial user model

---
+++ ultimate-data-scraper/models/user.py
@@ -0,0 +1,8 @@
+# User Model
+class User:
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+        
+# Add your database connection and methods here
